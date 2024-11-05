c66ed3bf964bb7213f705efd5254db0bd7f88c05
Seren Liu <SerenL2001@outlook.com>
2024-11-05 22:39:32 +1100

    add empty tab clickable on child menu of every shop menu

---
+++ ShopTabs/CatagoryMenu.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Menus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopTabs
+{
+    internal class CatagoryMenu : IClickableMenu
+    {
+        public ClickableTextureComponent testTab;
+
+        public CatagoryMenu(ShopMenu menu)
+        {
+            menu.SetChildMenu(this);
+            Dictionary<ISalable, ItemStockInformation> itemDict = menu.itemPriceAndStock;
+            Console.WriteLine(menu.xPositionOnScreen + ", "+  menu.yPositionOnScreen);
+            testTab = new ClickableTextureComponent("Recipes", 
+                new Rectangle(menu.xPositionOnScreen, menu.yPositionOnScreen - 60, 64, 64), 
+                "", 
+                "Recipes", 
+                Game1.mouseCursors, 
+                new Rectangle(16, 368, 16, 16), 
+                4f);
+        }
+
+        public override void draw(SpriteBatch b)
+        {
+            base.draw(b);
+            testTab.draw(b);
+        }
+
+        public override void performHoverAction(int x, int y)
+        {
+            base.performHoverAction(x, y);
+            testTab.tryHover(x, y);
+        }
+    }
+}
--- ShopTabs/ModEntry.cs
+++ ShopTabs/ModEntry.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
+using StardewValley.Menus;
+using System.Reflection.Metadata;
+using Microsoft.Xna.Framework.Content;
 
 namespace ShopTabs
 {
     /// <summary>The mod entry point.</summary>
     internal sealed class ModEntry : Mod
     {
+        public static PerScreen<CatagoryMenu> CatagoryMenuList = new PerScreen<CatagoryMenu>();
+
         /*********
         ** Public methods
         *********/
@@ -17,24 +23,28 @@ namespace ShopTabs
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            helper.Events.Display.MenuChanged += this.OnMenuChanged;
         }
 
 
         /*********
         ** Private methods
         *********/
-        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+
+        /// <summary>
+        /// Logs when a menu change.
+        /// </summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event data.</param>
-        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        private void OnMenuChanged(object? sender, MenuChangedEventArgs e)
         {
-            // ignore if player hasn't loaded a save yet
-            if (!Context.IsWorldReady)
-                return;
-
-            // print button presses to the console window
-            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+            if (Game1.activeClickableMenu != null && e.NewMenu is ShopMenu menu)
+            {
+                this.Monitor.Log($"Shop menu opened.", LogLevel.Debug);
+                CatagoryMenuList.Value = new CatagoryMenu(menu);
+            }
+            
         }
+
     }
 }
