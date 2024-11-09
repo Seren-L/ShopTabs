c253fc9b00b9c42623e3dbdbed9321bd3b2fda76
Seren Liu <SerenL2001@outlook.com>
2024-11-10 00:06:29 +1100

    tabmenu and helper methods

---
+++ ShopTabs/TabMenu.cs
@@ -0,0 +1,28 @@
+﻿using StardewValley.Menus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopTabs
+{
+    internal class TabMenu : IClickableMenu
+    {
+        public static List<string> catagories = new List<string>
+        {
+            "Forage", // type = Basic, catagory == -81
+            "Seeds", // type == Seeds, category == -74
+            "Saplings", // type == Basic, catagory == -74
+            "Cooking", // type == Cooking, catagory == -7
+            "Animal Product", // type == Basic, catagory == -6 for milk, -5 for eggs
+            "Fish", // type == fish, catagory == -4
+            "Artisan", // type == Basic, catagory == -26 for usual artisan, -27 for tree tap products
+            "Fertillizer", // type == Basic, catagory == -19
+            "Crops", // type == Basic, catagory == -75
+            "Recipes",
+            "Other"
+        };
+        public TabMenu() { }
+    }
+}
